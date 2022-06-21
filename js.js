18bc6a7edb2b9d3dd9234da2affb1bce4b2e0f99
Debi Codriansky <debicps@gmail.com>
2022-06-21 14:14:57 +0300

    Milestone4

--- js.js
+++ js.js
@@ -1,6 +1,35 @@
+const urlServer = "http://localhost:5050/fibonacci/" ;
+
+
+document.getElementById("btn").addEventListener("click", fibonacciServer);
+
+
+function fibonacciServer(){
+    let userNum = document.getElementById("myText").value;
+    let fullUrl= urlServer + userNum;
+fetch(fullUrl)
+.then(response => response.json ())
+.then(data => { 
+    document.getElementById("demo").innerText = data.result;
+});
+
+
+}
+
+
+
+/* when i click on the buttoin it activate the function 
+
+fibonacciServer(fullUrl);*/
+
+
+/*
+
+fibonacciServer(fullUrl);
+
 
 function fibonacci(){
-
+       
     let x = document.getElementById("myText").value;
 
     let num1= 0;
@@ -18,5 +47,8 @@ function fibonacci(){
 
     return num3;
 }
+*/
+
+
 
 
