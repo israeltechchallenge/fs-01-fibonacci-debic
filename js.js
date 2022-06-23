98f2079a1b86b24e4947a27e619b4af669e22269
Debi Codriansky <debicps@gmail.com>
2022-06-23 15:07:39 +0300

    Milestone5

--- js.js
+++ js.js
@@ -1,54 +1,57 @@
 const urlServer = "http://localhost:5050/fibonacci/" ;
+document.getElementById("btn").addEventListener("click", fibonacciServer );
+let errorMesage = document.getElementById("error");
 
+//document.getElementById("btn").addEventListener("click", spinner);
 
-document.getElementById("btn").addEventListener("click", fibonacciServer);
-
+function spinnerOn() {
+    var element = document.getElementById("spiner");
+    element.classList.add("d-block")
+    element.classList.remove("d-none");
+ }
+ function spinnerOff() {
+    var element = document.getElementById("spiner");
+    element.classList.add("d-none");
+ }
+ spinnerOff()
 
 function fibonacciServer(){
     let userNum = document.getElementById("myText").value;
-    let fullUrl= urlServer + userNum;
-fetch(fullUrl)
-.then(response => response.json ())
-.then(data => { 
-    document.getElementById("demo").innerText = data.result;
-});
+    let demo = document.getElementById("demo");
+    let error42 = document.getElementById("error42");
+    let error0 = document.getElementById("error0");
 
-
-}
-
-
-
-/* when i click on the buttoin it activate the function 
-
-fibonacciServer(fullUrl);*/
-
-
-/*
-
-fibonacciServer(fullUrl);
-
-
-function fibonacci(){
-       
-    let x = document.getElementById("myText").value;
-
-    let num1= 0;
-    let num2= 1;
-    let num3;
-    
-    for (i = 2; i <= x; i++){
-    num3= num1 + num2;
-    num1 = num2;
-    num2 = num3;
-
+    if(userNum>50){
+        demo.innerHTML = ``;
+        error0.innerHTML = ``;
+        error42.innerHTML = ``;
+        errorMesage.innerHTML += `<p class="error p-1 m-0 rounded mt-3">Cant be larger than 50</p>`;
+    } 
+      else if (userNum == 42){
+        demo.innerHTML = ``;
+        error0.innerHTML = ``;
+        errorMesage.innerText = ``;
+        error42.innerHTML += `<p style=" color: #D9534F;">Server Error: 42 is the meaning of life</p>`;  
+    }  
+      else if (userNum <= 0){
+        error42.innerHTML = ``;
+        demo.innerHTML = ``;
+        errorMesage.innerText = ``;
+        error0.innerHTML += `<p style=" color: #D9534F;" >Server Error</p>`;
     }
-    
-    document.getElementById("demo").innerHTML = num3;
-
-    return num3;
+        else {
+        error0.innerHTML = ``;
+        error42.innerHTML = ``;
+        errorMesage.innerText = ``;
+        let fullUrl= urlServer + userNum;
+        spinnerOn()
+        fetch(fullUrl)
+        .then(response => response.json ())
+        .then(data => { 
+        spinnerOff()
+        document.getElementById("demo").innerText = data.result;
+        });
+    }
 }
-*/
-
-
 
 
