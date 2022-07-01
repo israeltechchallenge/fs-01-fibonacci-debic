aaefd0ab68a9810eac350c8f7f1291018d0edb24
Debi Codriansky <debicps@gmail.com>
2022-07-01 13:25:35 +0300

    Final :)

--- js.js
+++ js.js
@@ -1,11 +1,14 @@
 const urlServer = "http://localhost:5050/fibonacci/" ;
 const urlResultsServer = "http://localhost:5050/getFibonacciResults " ;
 
-
-document.getElementById("btn").addEventListener("click", fibonacciServer );
+document.getElementById("btn").addEventListener("click", ckeckboxFunction );
 let errorMesage = document.getElementById("error");
 
-//document.getElementById("btn").addEventListener("click", spinner);
+window.onload = () => {
+    resultServer()
+    spinnerOff()
+    ckeckboxFunction()}
+
 
 function spinnerOn() {
     var element = document.getElementById("spiner");
@@ -20,7 +23,6 @@ function spinnerOn() {
  function resultServer(){
 
     let resultServerVar = document.getElementById("resultServer");
-
     fetch(urlResultsServer)
     .then(response => response.json ())
     .then(data => {
@@ -31,18 +33,12 @@ function spinnerOn() {
            
             text +=  `<p class="py-3 border1">The Fibonacci Of <strong>${boxResult[i].number}</strong> is <strong>${boxResult[i].result}</strong> calculated at <strong>${new Date(boxResult[i].createdDate)}</strong></p> `;
             document.getElementById("resultServer").innerHTML = text
-         console.log(resultServerVar)
       
         }
     }
     )
 }
 
-
-
-window.onload = () => {
- resultServer()
- spinnerOff()}
 
 function fibonacciServer(){
 
@@ -86,3 +82,56 @@ function fibonacciServer(){
         });
     }
 }
+
+
+function fibonacci(){
+
+    let userNum = document.getElementById("myText").value;
+    let demo = document.getElementById("demo");
+    let error42 = document.getElementById("error42");
+    let error0 = document.getElementById("error0");
+    let num1= 0;
+    let num2= 1;
+    let num3;
+ 
+
+    if(userNum>50){
+        demo.innerHTML = ``;
+        error0.innerHTML = ``;
+        error42.innerHTML = ``;
+        errorMesage.innerHTML += `<p class="error p-1 m-0 rounded mt-3">Cant be larger than 50</p>`;
+    } 
+      else if (userNum == 42){
+        demo.innerHTML = ``;
+        error0.innerHTML = ``;
+        errorMesage.innerText = ``;
+        error42.innerHTML += `<p style=" color: #D9534F;">Server Error: 42 is the meaning of life</p>`;  
+    }  
+      else if (userNum < 0){
+        error42.innerHTML = ``;
+        demo.innerHTML = ``;
+        errorMesage.innerText = ``;
+        error0.innerHTML += `<p style=" color: #D9534F;" >Server Error</p>`;
+    }else{
+        for (i = 2; i <= userNum; i++){
+            num3= num1 + num2;
+            num1 = num2;
+            num2 = num3;
+
+    }
+    document.getElementById("demo").innerHTML = num3;
+    return num3;
+}
+
+    }
+
+
+
+function ckeckboxFunction() {
+    var checkBox = document.getElementById("myCheck");
+    if (checkBox.checked == true){
+        fibonacciServer()
+    } else {
+        fibonacci()
+    }
+  }
